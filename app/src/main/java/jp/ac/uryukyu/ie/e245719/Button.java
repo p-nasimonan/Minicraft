838599a297b7ecdab494b3eea7c83fbfc8794080
P-nasi <pnasimonan@gmail.com>
2025-01-17 22:43:05 +0900

    Update Button.java

--- app/src/main/java/jp/ac/uryukyu/ie/e245719/Button.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245719/Button.java
@@ -2,11 +2,23 @@ package jp.ac.uryukyu.ie.e245719;
 
 import org.lwjgl.opengl.GL11;
 
+/**
+ * ゲーム内のボタンを表現するクラス。
+ * スタート画面などのUIで使用されます。
+ */
 public class Button {
     private final float x, y, width, height;
     private final String label;
     private boolean hovered;
 
+    /**
+     * ボタンを初期化します。
+     * @param x ボタンのX座標
+     * @param y ボタンのY座標
+     * @param width ボタンの幅
+     * @param height ボタンの高さ
+     * @param label ボタンのラベル
+     */
     public Button(float x, float y, float width, float height, String label) {
         this.x = x;
         this.y = y;
@@ -16,6 +28,10 @@ public class Button {
         this.hovered = false;
     }
 
+    /**
+     * ボタンを画面に描画します。
+     * 背景とアイコンを描画します。
+     */
     public void render() {
         // ボタンの背景を描画
         GL11.glColor3f(hovered ? 0.7f : 0.5f, 0.5f, 0.5f);
@@ -38,18 +54,36 @@ public class Button {
         GL11.glEnd();
     }
 
+    /**
+     * マウスがボタン上にあるかどうかを判定します。
+     * @param mouseX マウスのX座標
+     * @param mouseY マウスのY座標
+     * @return ボタン上にマウスがある場合はtrue
+     */
     public boolean isTouched(double mouseX, double mouseY) {
         return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
     }
 
+    /**
+     * ボタンのラベルを取得します。
+     * @return ボタンのラベル文字列
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * ボタンがホバー状態かどうかを取得します。
+     * @return ホバー状態の場合はtrue
+     */
     public boolean isHovered() {
         return hovered;
     }
 
+    /**
+     * ボタンのホバー状態を設定します。
+     * @param hovered 設定するホバー状態
+     */
     public void setHovered(boolean hovered) {
         this.hovered = hovered;
     }
